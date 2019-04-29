f8fefac5efa9696c603408c0eed0179e15e41c26
Adam Spruijt <adamspruijt@gmail.com>
2019-04-29 16:44:17 -0400

    cleanup for release

--- cashbox.js
+++ cashbox.js
@@ -3,7 +3,6 @@ $.fn.cashbox = function(options) {
     // default options.
     var settings = $.extend(
         {
-            debug: false,
             placeholder: "<div class='cashbox--placeholder'></div>",
             toggleClass: "cashbox--is-hidden",
             gallery: true,
@@ -36,12 +35,19 @@ $.fn.cashbox = function(options) {
     };
 
     return this.each(function() {
-        var $item = $(this);
+        var $this = $(this);
         var $img = $("<img>").addClass(settings.classes.image);
         
-        $item.on("click", function(event) {
+        // get data attributes
+        if ($this.data("cashbox-target")) {
+            var target = $this.data("cashbox-target");
+            console.log(target);
+        }
+
+
+        $this.on("click", function(event) {
             event.preventDefault();
-            var imageSrc = $item.attr("href");
+            var imageSrc = $this.attr("href");
             $body.addClass(settings.classes.bodyOpenClass);
             $placeholder.toggleClass(settings.classes.placeholderToggleClass);
             $img.attr("src",imageSrc);
